38bb4bf6fe98676765315f81cdbb8fb4f0502eba
Maksim Masterenko <mmasterenko@gmail.com>
2015-10-27 13:45:49 +0300

    portfolio page create

--- pages/views.py
+++ pages/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 
 def main(req):
     return render(req, 'pages/index.html')
+
+
+def portfolio(req, n=None):
+    context = {'n': n}
+    return render(req, 'pages/portfolio.html', context=context)
+
--- svp/urls.py
+++ svp/urls.py
@@ -4,5 +4,7 @@ from pages import views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', views.main, name='main')
+    url(r'^$', views.main, name='main'),
+    url(r'^portfolio/$', views.portfolio, name='portfolio'),
+    url(r'^portfolio/([0-9]+)/$', views.portfolio, name='portfolio')
 ]
